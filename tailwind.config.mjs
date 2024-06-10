397e30f668b312a7bf1836d86dd76cd2df1c31e5
Nardis <nardis9501@gmail.com>
2024-06-10 10:55:05 -0400

    feat: add brand color palette to Tailwind configuration
    - Updated tailwind.config.js with brand color palette.
    - Added primary, secondary, and accent colors to the theme.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: '#000000',
+			white: '#ffffff',
+			gray: colors.gray,
+			indigo: colors.indigo,
+			neutral: colors.neutral, // Used mainly for text color
+			salmon: {
+				50: '#FDF4F2',
+				100: '#FAE5E0',
+				400: '#EB9B89',
+				500: '#E6816B'
+			}, // Accent colors, used mainly for star color, heading and buttons
+			blue: {
+				100: '#C2E3FA',
+				200: '#85C8F5',
+				300: '#48ACEF',
+				400: '#138FE2',
+				500: '#0E67A4',
+				600: '#0B5384'
+			}, // Primary colors, used mainly for links, buttons and svg icons
+			red: colors.red, // Used for bookmark icon
+			zinc: colors.zinc // Used mainly for box-shadow
+		},
+		extend: {}
 	},
-	plugins: [],
+	plugins: []
 }
